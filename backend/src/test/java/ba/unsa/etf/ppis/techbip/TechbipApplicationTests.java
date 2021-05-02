dfc3c51eb808818405f81d1556083714a91f7f9a
kkadusic <kkadusic1@etf.unsa.ba>
2021-05-02 21:28:23 +0200

    Add initial Spring Boot structure

---
+++ backend/src/test/java/ba/unsa/etf/ppis/techbip/TechbipApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.ppis.techbip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
