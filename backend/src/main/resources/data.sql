7fe2a88a1f9b5c4799a8a8aeae1c9825cc6a5fd9
kkadusic <kkadusic1@etf.unsa.ba>
2021-05-24 14:13:26 +0200

    Update data.sql, remove unused variable

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -9,7 +9,7 @@ INSERT INTO public.employee_role (employee_id, role_id) VALUES (501, 0);
 
 INSERT INTO public.education (id, school, degree, field_of_study, start_date, end_date, employee_id) VALUES (500, 'Harvard', 'Bachelor', 'Electrical Engineering', '1980-09-22 13:35:18.847706', '1983-09-22 13:35:18.847706', 500);
 INSERT INTO public.education (id, school, degree, field_of_study, start_date, end_date, employee_id) VALUES (501, 'MIT', 'Bachelor', 'Computer Science', '1985-09-22 13:35:18.847706', '1988-09-22 13:35:18.847706', 501);
-INSERT INTO public.education (id, school, degree, field_of_study, start_date, end_date, employee_id) VALUES (502, 'MIT', 'Master', 'Computer Science', '1985-09-22 13:35:18.847706', '1990-09-22 13:35:18.847706', 501);
+INSERT INTO public.education (id, school, degree, field_of_study, start_date, end_date, employee_id) VALUES (502, 'MIT', 'Master', 'Computer Science', '1988-09-22 13:35:18.847706', '1990-09-22 13:35:18.847706', 501);
 
 INSERT INTO public.certificate (id, name, issuing_organization, issue_date, employee_id) VALUES (500, 'Advanced Databases', 'Stanford', '2005-09-22 13:35:18.847706', 500);
 INSERT INTO public.certificate (id, name, issuing_organization, issue_date, employee_id) VALUES (501, 'Java Fundamentals', 'Udemy', '2007-09-22 13:35:18.847706', 500);
--- frontend/src/components/Edukacija/ListaEdukacijaPregled.js
+++ frontend/src/components/Edukacija/ListaEdukacijaPregled.js
@@ -4,18 +4,8 @@ import Edukacija from '../Rjesenje/Edukacija';
 import {useLocation} from "react-router-dom";
 
 const ListaEdukacijaPregled = (props) => {
-
-    let edukacija = {
-        ustanova: 'UNSA ETF',
-        stepen: "Bachelor",
-        oblast: "Informatika",
-        datumPocetka: "01/01/2018",
-        datumKraja: "01/01/2021"
-    };
-
     const location = useLocation();
     const educations = location.state.educations;
-    console.log(educations);
 
     return (
         <div className="grid-certifikata">
