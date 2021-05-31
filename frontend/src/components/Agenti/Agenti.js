8636a689cb36fcfd54bde36ab7e2ad9c428bd04b
kkadusic <kkadusic1@etf.unsa.ba>
2021-05-31 13:38:27 +0200

    Add loading spinner

--- frontend/src/components/Agenti/Agenti.js
+++ frontend/src/components/Agenti/Agenti.js
@@ -2,16 +2,20 @@ import React, {useEffect, useState} from 'react';
 import AgentKartica from './AgentKartica';
 import Pretraga from '../Pretraga/Pretraga';
 import {getEmployees} from "../../utilities/serverCall";
-import {message} from 'antd';
+import {message, Spin} from 'antd';
 
 const Agenti = () => {
     const [agents, setAgents] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
+                setLoading(true);
                 setAgents(await getEmployees());
+                setLoading(false);
             } catch (error) {
+                setLoading(false);
                 message.warning(error.response.data.message, 3);
             }
         }
@@ -21,11 +25,13 @@ const Agenti = () => {
     return (
         <div className="prozor">
             <Pretraga/>
-            <div className="zahtjevi-grid">
-                {agents.map(agent => (
-                    <AgentKartica key={agent.id} agent={agent}/>
-                ))}
-            </div>
+            {loading ? <Spin size="large"/> :
+                <div className="zahtjevi-grid">
+                    {agents.map(agent => (
+                        <AgentKartica key={agent.id} agent={agent}/>
+                    ))}
+                </div>
+            }
             <button className="lijevi">&lt;&lt;</button>
             <button className="desni">&gt;&gt;</button>
         </div>
